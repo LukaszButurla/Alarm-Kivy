14f7622c63cf98a6ccfcedbfaafa245ff483796c
DESKTOP-HF35O0S\lukasz <lukasz.buturla.25@gmail.com>
2022-07-19 15:42:53 +0200

    Create timer buttons

--- Timer/timer.py
+++ Timer/timer.py
@@ -1,6 +1,21 @@
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
 
 
 class TimerWidget(FloatLayout):
     
-    pass
+    timerActive = False
+    
+    def start_stop_timer(self):
+        
+        if self.timerActive == False:
+            self.timerActive = True
+            self.ids.btnStartStopId.pos_hint = {"x": 0.6, "y": 0.4}
+            self.ids.btnPauseId.opacity = 1
+            
+        else:
+            self.timerActive = False
+            self.ids.btnStartStopId.pos_hint = {"x": 0.4, "y": 0.4}
+            self.ids.btnPauseId.opacity = 0
+            
+        
