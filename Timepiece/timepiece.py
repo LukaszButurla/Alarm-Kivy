4decef1ae25036e5d79d2c9c050b71c9d308b0e6
DESKTOP-HF35O0S\lukasz <lukasz.buturla.25@gmail.com>
2022-07-20 15:24:59 +0200

    Create function to add and minus time in timepiece

--- Timepiece/timepiece.py
+++ Timepiece/timepiece.py
@@ -2,4 +2,50 @@ from kivy.uix.floatlayout import FloatLayout
 
 class TimepieceWidget(FloatLayout):
     
-    pass
+    selectedHour = 0
+    selectedMinute = 0
+    selectedSecond = 0
+    
+    def add(self, time):
+        
+        match time:
+            case "hour":
+                if self.selectedHour == 23:
+                    self.selectedHour = 0
+                else:
+                    self.selectedHour += 1
+            case "minute":
+                if self.selectedMinute == 59:
+                    self.selectedMinute = 0
+                else:
+                    self.selectedMinute += 1
+            case "second":
+                if self.selectedSecond == 59:
+                    self.selectedSecond = 0
+                else:
+                    self.selectedSecond += 1
+                    
+        print(self.selectedHour, self.selectedMinute, self.selectedSecond)
+                    
+    def minus(self, time):
+        
+        match time:
+            case "hour":
+                if self.selectedHour == 0:
+                    self.selectedHour = 23
+                else:
+                    self.selectedHour -= 1
+            case "minute":
+                if self.selectedMinute == 0:
+                    self.selectedMinute = 59
+                else:
+                    self.selectedMinute -= 1
+            case "second":
+                if self.selectedSecond == 0:
+                    self.selectedSecond = 59
+                else:
+                    self.selectedSecond -= 1
+                    
+        print(self.selectedHour, self.selectedMinute, self.selectedSecond)
+                    
+                    
