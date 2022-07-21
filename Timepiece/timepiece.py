04b5c6c72a645ad22c703a1843fa24ec7ef97548
DESKTOP-HF35O0S\lukasz <lukasz.buturla.25@gmail.com>
2022-07-21 12:33:12 +0200

    Create function to show selected time in label

--- Timepiece/timepiece.py
+++ Timepiece/timepiece.py
@@ -1,51 +1,61 @@
 from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import StringProperty
 
 class TimepieceWidget(FloatLayout):
     
     selectedHour = 0
     selectedMinute = 0
     selectedSecond = 0
+    selectedTime = StringProperty("00:00.00")
     
     def add(self, time):
         
         match time:
             case "hour":
-                if self.selectedHour == 23:
-                    self.selectedHour = 0
-                else:
-                    self.selectedHour += 1
+                self.selectedHour = int(self.selectedHour) + 1
             case "minute":
-                if self.selectedMinute == 59:
+                if int(self.selectedMinute) == 59:
                     self.selectedMinute = 0
                 else:
-                    self.selectedMinute += 1
+                    self.selectedMinute = int(self.selectedMinute) + 1
             case "second":
-                if self.selectedSecond == 59:
+                if int(self.selectedSecond) == 59:
                     self.selectedSecond = 0
                 else:
-                    self.selectedSecond += 1
+                    self.selectedSecond = int(self.selectedSecond) + 1
                     
-        print(self.selectedHour, self.selectedMinute, self.selectedSecond)
+        self.check_len()                    
+        self.selectedTime = "{}:{}.{}".format(self.selectedHour, self.selectedMinute, self.selectedSecond)
                     
     def minus(self, time):
         
         match time:
             case "hour":
-                if self.selectedHour == 0:
-                    self.selectedHour = 23
-                else:
-                    self.selectedHour -= 1
+                if int(self.selectedHour) > 0:
+                    self.selectedHour = int(self.selectedHour) - 1
             case "minute":
-                if self.selectedMinute == 0:
+                if int(self.selectedMinute) == 0:
                     self.selectedMinute = 59
                 else:
-                    self.selectedMinute -= 1
+                    self.selectedMinute = int(self.selectedMinute) - 1
             case "second":
-                if self.selectedSecond == 0:
+                if int(self.selectedSecond) == 0:
                     self.selectedSecond = 59
                 else:
-                    self.selectedSecond -= 1
+                    self.selectedSecond = int(self.selectedSecond) - 1
                     
-        print(self.selectedHour, self.selectedMinute, self.selectedSecond)
+        self.check_len()          
+        self.selectedTime = "{}:{}.{}".format(self.selectedHour, self.selectedMinute, self.selectedSecond)
+        
+    def check_len(self):
+        
+        if len(str(self.selectedHour)) < 2:
+            self.selectedHour = "0" + str(self.selectedHour)
+            
+        if len(str(self.selectedMinute)) < 2:
+            self.selectedMinute = "0" + str(self.selectedMinute)
+            
+        if len(str(self.selectedSecond)) < 2:
+            self.selectedSecond = "0" + str(self.selectedSecond)
                     
                     
