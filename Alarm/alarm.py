07d2ca6fd47f6fae7c8d2cc98f70e1b5f2f52dd1
DESKTOP-HF35O0S\lukasz <lukasz.buturla.25@gmail.com>
2022-07-22 13:53:39 +0200

    Create function to print alarms from file

--- Alarm/alarm.py
+++ Alarm/alarm.py
@@ -1,5 +1,29 @@
 from kivy.uix.boxlayout import BoxLayout
+import os
 
 class AlarmWidget(BoxLayout):
     
-    pass
+    filePath = r"C:\kivygui\Alarm\Alarm\files\alarms.txt"
+    
+    def __init__(self, **kwargs):
+        self.show_alarms()
+        super().__init__(**kwargs)
+    
+    def show_alarms(self):
+        
+        dirExist = os.path.exists(self.filePath)
+                
+        if dirExist:
+            with open(self.filePath, "r", encoding="utf-8") as fileOpen:
+                lines = fileOpen.readlines()
+                
+                for line in lines:
+                    line = line.split(";")
+                    alarmHour = line[0]
+                    alarmMinute = line[1]
+                    alarmDays = line[2]
+                    alarmColor = line[3]
+                    alarmDescription = line[4]
+                    
+                    print("hour - {}\nminute - {}\ndays - {}\ncolor - {}\ndes - {}\n".format(alarmHour, alarmMinute, alarmDays, alarmColor, alarmDescription))
+                    
